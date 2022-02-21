9b95b299d8c85b34626f0f0df2c78ddd6d75a2e9
juanriudala <juanriudala@estudiante.edib.es>
2022-02-21 11:07:10 +0100

    Subida de archivos

---
+++ RangoIncorrectoException.java
@@ -0,0 +1,13 @@
+package pro9;
+
+public class RangoIncorrectoException extends RuntimeException {
+	
+	public RangoIncorrectoException() {
+		super();
+	}
+	
+	public RangoIncorrectoException(String msg) {
+		super(msg);
+	}
+
+}
